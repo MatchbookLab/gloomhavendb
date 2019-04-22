38fc67aeb5ee4cdf705821379f66cb40d3e0ad88
Cameron Spear <cameronspear@gmail.com>
2019-04-22 01:30:29 -0700

    Some fun with items

--- src/server/api/item/item.controller.ts
+++ src/server/api/item/item.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { items } from '../../../data/items';
+import { Item } from '../../../shared/entities/item';
+import { ItemRepository } from './item.repository';
+import { sortBy } from 'lodash';
 
 @Controller('items')
-export class ItemController {}
+export class ItemController {
+  constructor(private itemRepo: ItemRepository) {
+    this.seed();
+  }
+
+  @Get()
+  async getItems() {
+    return sortBy(await this.itemRepo.smartFind(), 'number');
+  }
+
+  @Get(':id')
+  async findItem(@Param('id') id: string) {
+    return this.itemRepo.smartFindOneOrFail(id);
+  }
+
+  @Post()
+  async createItem(@Body() item: Item) {
+    item = this.itemRepo.scrub(item);
+    return await this.itemRepo.saveNew(item);
+  }
+
+  @Put(':id')
+  async updateItem(@Param('id') id: string, @Body() item: Item) {
+    item = this.itemRepo.scrub(item);
+    return await this.itemRepo.saveExisting(id, item);
+  }
+
+  @Delete(':id')
+  async deleteItem(@Param('id') id: string) {
+    return await this.itemRepo.deleteAndReturn(id);
+  }
+
+  private async seed() {
+    if ((await this.itemRepo.smartFind()).length) {
+      return;
+    }
+
+    await this.itemRepo.save(items);
+  }
+}
--- src/shared/entities/item.ts
+++ src/shared/entities/item.ts
@@ -8,17 +8,18 @@ export class Item {
   // this is the item number and is _not_ generated
   @PrimaryColumn() number: number;
   // the little number at the bottom of the front (this is unique per card and not really helpful here)
-  /* @deprecated */ cardNo: number;
+  /* @deprecated */
+  cardNo: number;
   @Column() name: string;
   @Column('enum', { enum: Slot }) slot: Slot;
   @Column() price: number;
   @Column() text: string;
-  @Column() count: number;
-  @Column('enum', { enum: Limit }) limit: Limit;
-  @Column() uses: number | null;
-  @Column() negativeCardsCount: number | null;
-  @Column('enum', { enum: ItemSource }) sourceType: ItemSource;
-  @Column() sourceId: number | null;
+  @Column({ nullable: true /* hopefully temp */ }) count: number;
+  @Column('enum', { enum: Limit, nullable: true /* hopefully temp */ }) limit: Limit;
+  @Column({ nullable: true }) uses: number | null;
+  @Column({ nullable: true }) negativeCardsCount: number | null;
+  @Column('enum', { enum: ItemSource, nullable: true }) sourceType: ItemSource;
+  @Column({ nullable: true }) sourceId: number | null;
   @Column() imageUrl: string;
   @Column() verified: boolean;
 }
