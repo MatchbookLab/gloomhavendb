320dde56067167385a96f89bc7a6e560a5eda18e
Cameron Spear <cameronspear@gmail.com>
2019-05-08 11:55:59 -0700

    Even more refinements for SVG icons; inline SVG now and inherit font color

--- src/client/app/shared/icon/icon.module.ts
+++ src/client/app/shared/icon/icon.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 import { IconComponent } from './icon.component';
 
 const declareAndExport = [IconComponent];
 
 @NgModule({
+  imports: [AngularSvgIconModule],
   declarations: declareAndExport,
   exports: declareAndExport,
 })
