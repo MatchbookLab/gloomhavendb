858f4c23845ee96b4942e7346e1751be1fa8b8ee
Cameron Spear <cameronspear@gmail.com>
2019-05-08 17:59:58 -0700

    Edits from the phone are dangerous

--- e2e/cypress/integration/home.e2e.ts
+++ e2e/cypress/integration/home.e2e.ts
@@ -2,6 +2,6 @@ describe('Home Page', () => {
   it('should work', () => {
     cy.visit('http://localhost:4200');
 
-    cy.get('.contain').should('contain', 'Future home of the Gloomhaven DB');
+    cy.get('.content').should('contain', 'Future home of the Gloomhaven DB');
   });
 });
