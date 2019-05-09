e477da4cb0f9c65fcd1f64225d10438c62bd5667
Cameron Spear <cameronspear@gmail.com>
2019-05-08 17:59:28 -0700

    Fix selector after HTML change.

--- e2e/cypress/integration/home.e2e.ts
+++ e2e/cypress/integration/home.e2e.ts
@@ -2,6 +2,6 @@ describe('Home Page', () => {
   it('should work', () => {
     cy.visit('http://localhost:4200');
 
-    cy.get('.body').should('contain', 'Future home of the Gloomhaven DB');
+    cy.get('.contain').should('contain', 'Future home of the Gloomhaven DB');
   });
 });
