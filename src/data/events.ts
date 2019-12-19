c801f201134ea69e3e44dcf9fcb4bfcac0abf246
Oliver Schrenk <oliver.schrenk@gmail.com>
2019-12-19 13:31:47 +0100

    Road 1

--- src/data/events.ts
+++ src/data/events.ts
@@ -1711,17 +1711,17 @@ export const events: EventEntity[] = [
     type: EventType.Road,
     number: 1,
     text:
-      "*7hading out a little late, aren't you? The guard at the wall looks at you passively. You grunt in response and head through the opened gate, \"Nobody s gonna go looking Tor your corpse ifyou don't returnf* the guard shouts at your back. You ended up embarking out on the road much later than you had hoped events in town saw to that — but as dusk settles on the horizon you feel confident that you are up to any threat you might face. And then begins the howling of wolves — vicious, foul beasts — and, judging from their sounds, they seem to be getting closer.",
+      '"Heading out a little late, aren\'t you?" The guard at the wall looks at you passively. You grunt in response and head through the opened gate.\n\n"Nobody\'s gonna go looking for your corpse if you don\'t return!" the guard shouts at your back.\n\nYou ended up embarking out on the road much later than you had hoped — events in town saw to that — but as dusk settles on the horizon you feel confident that you are up to any threat you might face.\n\nAnd then begins the howling of wolves — vicious, foul beasts — and, judging from their sounds, they seem to be getting closer.',
     optionA: {
       choice: 'Run from the howling to safety.',
       outcome:
-        '1 You pick up and run as fast as you can through the underbrush and away from the ominous sounds. They seem to be receding as you stumble headlong into some sort o f thicket hiled with sticky spines. You pull yourself out, but not before your skin is pierced in numerous places and becomes in flamed. Best to avoid this plant in the future. All start scenario with POISON^ •',
+        'You pick up and run as fast as you can through the underbrush and away from the ominous sounds. They seem to be receding as you stumble headlong into some sort of thicket filled with sticky spines.\n\nYou pull yourself out, but not before your skin is pierced in numerous places and becomes inflamed. Best to avoid this plant in the future.\n\nAll start scenario with {Poison}',
       imageUrl: '/assets/cards/events/base/road/re-01-b-a.png',
     },
     optionB: {
       choice: 'Let the wolves come.',
       outcome:
-        'Confident that the wolves pose no significant threat, you stand your ground and prepare for battle, The pack comes — ragged and hungry slowly emerging from the dark — and surrounds your party; There are more of them than you expected[ but not enough to take you down, You suffer a bite or iwot but are able to fight them off. All start scenario with 3 damage.',
+        'Confident that the wolves pose no significant threat, you stand your ground and prepare for battle, The pack comes — ragged and hungry, slowly emerging from the dark — and surrounds your party.\n\nThere are more of them than you expected but not enough to take you down. You suffer a bite or two but are able to fight them off.\n\nAll start scenario with 3 damage.',
       imageUrl: '/assets/cards/events/base/road/re-01-b-b.png',
     },
     imageUrl: '/assets/cards/events/base/road/re-01-f.png',
